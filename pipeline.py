861bd7d01dd07d0fda2492ca6c8d3860b45ba9c7
arkiver <arkiver@protonmail.com>
2025-08-05 13:33:31 +0200

    Version 20250805.01. Use HTTPS instead of HTTP.

--- pipeline.py
+++ pipeline.py
@@ -77,7 +77,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250715.02'
+VERSION = '20250804.01'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
 TRACKER_ID = 'metaadlibrary'
 TRACKER_HOST = 'legacy-api.arpa.li'
@@ -369,14 +369,14 @@ project = Project(
     title=TRACKER_ID,
     project_html='''
         <img class="project-logo" alt="Project logo" src="https://wiki.archiveteam.org/images/thumb/8/80/Meta-icon.png/800px-Meta-icon.png" height="50px" title=""/>
-        <h2>Meta Ad Library <span class="links"><a href="https://www.facebook.com/ads/library/">Website</a> &middot; <a href="http://tracker.archiveteam.org/metaadlibrary/">Leaderboard</a> &middot; <a href="https://wiki.archiveteam.org/index.php/Meta_Ad_Library">Wiki</a></span></h2>
+        <h2>Meta Ad Library <span class="links"><a href="https://www.facebook.com/ads/library/">Website</a> &middot; <a href="https://tracker.archiveteam.org/metaadlibrary/">Leaderboard</a> &middot; <a href="https://wiki.archiveteam.org/index.php/Meta_Ad_Library">Wiki</a></span></h2>
         <p>Archiving the Meta Ad Library.</p>
     '''
 )
 
 pipeline = Pipeline(
     CheckIP(),
-    GetItemFromTracker('http://{}/{}/multi={}/'
+    GetItemFromTracker('https://{}/{}/multi={}/'
         .format(TRACKER_HOST, TRACKER_ID, MULTI_ITEM_SIZE),
         downloader, VERSION),
     PrepareDirectories(warc_prefix=TRACKER_ID),
@@ -407,7 +407,7 @@ pipeline = Pipeline(
         name='shared:rsync_threads', title='Rsync threads',
         description='The maximum number of concurrent uploads.'),
         UploadWithTracker(
-            'http://%s/%s' % (TRACKER_HOST, TRACKER_ID),
+            'https://%s/%s' % (TRACKER_HOST, TRACKER_ID),
             downloader=downloader,
             version=VERSION,
             files=[
@@ -424,7 +424,7 @@ pipeline = Pipeline(
         ),
     ),
     MaybeSendDoneToTracker(
-        tracker_url='http://%s/%s' % (TRACKER_HOST, TRACKER_ID),
+        tracker_url='https://%s/%s' % (TRACKER_HOST, TRACKER_ID),
         stats=ItemValue('stats')
     )
 )
